a04415099caf79ab06a881c0b2e8d6eb412e8472
Rimantas <belauzas@yahoo.com>
2019-11-12 15:27:49 +0200

    Rendering services with JavaScript

--- js/data.js
+++ js/data.js
@@ -9,6 +9,38 @@
 // portfolio
 
 // services
+const services = [
+    {
+        icon: 'facebook',
+        title: 'Interface Design',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'twitter',
+        title: 'Experience Design',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'instagram',
+        title: 'Web Development',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'linkedin',
+        title: 'Mobile App',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'facebook',
+        title: 'Digital Marketing',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'facebook',
+        title: 'Woocommerce',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    }
+];
 
 // team
 
