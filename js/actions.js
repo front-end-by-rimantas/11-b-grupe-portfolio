73e0e5f6f437688f16086640037ea298e8825671
Rimantas <belauzas@yahoo.com>
2019-11-18 14:18:16 +0200

    Header menu on tablet/mobile #5

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,19 @@
 "use strict";
 
 // header
+const hamburger = document.querySelector('#main_header .fa-bars');
+const close = document.querySelector('#main_header .fa-times');
+const header = document.querySelector('#main_header');
+
+// bevarde funkcija: function(){}
+hamburger.addEventListener('click', function(){
+    return header.classList.add('show-menu');
+});
+
+// arrow function: () => {}
+close.addEventListener('click', () => {
+    header.classList.remove('show-menu');
+});
 
 // hero
 
